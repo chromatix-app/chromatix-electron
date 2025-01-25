13cb71b00b57d72e05da176e7bf8cc0d34b1463c
Alex <abimpson@gmail.com>
2025-01-25 23:11:57 +0000

    Make electron platform available to front end

--- electron/preload.js
+++ electron/preload.js
@@ -8,21 +8,24 @@
 // DISPLAY SPECS ON FRONT END
 // ======================================================================
 
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
-});
+// // All of the Node.js APIs are available in the preload process.
+// // It has the same sandbox as a Chrome extension.
+// window.addEventListener('DOMContentLoaded', () => {
+//   const replaceText = (selector, text) => {
+//     const element = document.getElementById(selector);
+//     if (element) element.innerText = text;
+//   };
+//   for (const dependency of ['chrome', 'node', 'electron']) {
+//     replaceText(`${dependency}-version`, process.versions[dependency]);
+//   }
+// });
 
 // ======================================================================
 // MESSAGING BETWEEN FRONT AND BACK END
 // ======================================================================
 
 window.isElectron = true;
+window.electronProcess = {
+  platform: process.platform,
+};
 window.ipcRenderer = require('electron').ipcRenderer;
